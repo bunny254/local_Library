f3f72913fc3a74c3c9d61f12f0a90e55ee1fe527
bunny254 <kimaniwachira03@gmail.com>
2022-04-04 10:55:02 +0300

    basic routing

--- server/app.js
+++ server/app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var kinkyRouter= require('./routes/kinky');
+var coolRouter= require('./routes/cool');
+
 
 var app = express();
 
@@ -20,7 +23,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/dr', kinkyRouter);
 app.use('/users', usersRouter);
+app.use('/users/cool', coolRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ server/routes/kinky.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('Dr Fuzzy is really cool I admire her alot she has sense than most of these regular bitches!');
+});
+
+
+module.exports = router;
